4e2e39d0a2162c328c2e76ee4620595bf93df1c6
Alex Mcknight <mcknight816@gmail.com>
2016-12-15 08:21:30 -0500

    commit by:Catwalk

---
+++ docker/db/init-settings.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sed -i '/host    all             all             127.0.0.1\/32            trust/a host    all             all             192.168.1.1\/16            trust' /var/lib/postgresql/data/pg_hba.conf
---
+++ docker/db/init-sql.sql
@@ -0,0 +1,3 @@
+ALTER USER postgres WITH PASSWORD 'Stud1o4465';
+CREATE DATABASE martialarts;
+GRANT ALL PRIVILEGES ON DATABASE martialarts TO postgres;
